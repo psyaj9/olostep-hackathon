731c3c1cb74fa9cdf4ab0869397570d8c81c784a
psyaj9 <athikashjeyaganthan@yahoo.com>
2024-08-17 08:49:00 +0100

    made changes to server so that it stores scraped data to mongodb

--- server/server.js
+++ server/server.js
@@ -1,30 +1,67 @@
 const express = require('express');
 const { Builder, By, until } = require('selenium-webdriver');
+const { MongoClient } = require('mongodb');
+const axios = require('axios');
 const app = express();
 const port = 3000;
 
-async function scrapeData() {
+async function scrapeAndStore() {
+
+  const uri = 'mongodb://localhost:27017';
+  const client = new MongoClient(uri);
+
+  const url = 'https://en.wikipedia.org/wiki/2024_Summer_Olympics';
+  try {
+    const response = await axios.get(url);
+    if (response.status === 404) {
+      console.error('Error 404: Page not found');
+      return; 
+    }
+
+    await client.connect();
+
+    const database = client.db('webScrapingDB');
+    const collection = database.collection('scrapedData');
+
     let driver = await new Builder().forBrowser('chrome').build();
 
     try {
-        await driver.get('https://en.wikipedia.org/wiki/2024_Summer_Olympics');
+      await driver.get(url);
 
-        let element = await driver.wait(until.elementLocated(By.tagName('h1')), 10000);
+      let element = await driver.wait(until.elementLocated(By.tagName('h1')), 10000);
 
-        let h1Text = await element.getText();
+      let h1Text = await element.getText();
 
-        console.log('H1 Tag Content:', h1Text);
+      console.log('h1 Tag Content:', h1Text);
+
+      const dataToStore = { url: url, h1: h1Text, date: new Date() };
+      await collection.insertOne(dataToStore);
 
     } catch (error) {
-        console.error('Error:', error);
+      if (error.name === 'NoSuchElementError') {
+        console.error('Element not found on the page');
+      } else if (error.name === 'TimeoutError') {
+        console.error('Timed out waiting for the element');
+      } else {
+        console.error('Error during scraping:', error);
+      }
     } finally {
-
-        await driver.quit();
+      await driver.quit();
     }
+
+  } catch (err) {
+    if (err.response && err.response.status === 404) {
+      console.error('Error 404: Page not found)');
+    } else {
+      console.error('Error connecting to MongoDB or during Axios request:', err);
+    }
+  } finally {
+    await client.close();
+  }
 }
 
 app.get('/', (req, res) => {
-    res.send(scrapeData());
+    res.send(scrapeAndStore());
 });
 
 app.listen(port, () => {
